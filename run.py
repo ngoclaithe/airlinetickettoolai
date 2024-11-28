68af768e2c3e43a89366110b0c60b92eaa1f0ff5
laingoc99 <laingoc99@gmail.com>
2024-11-28 07:25:44 +0700

    update28112024lan2

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
 from app import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    serve(app, host="0.0.0.0", port=5000)
