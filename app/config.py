867ab7f55f94110a565b8df2d9906bdaedbb1ac3
laingoc99 <laingoc99@gmail.com>
2024-11-30 19:32:33 +0700

    update2

--- app/config.py
+++ app/config.py
@@ -4,4 +4,5 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.abspath('dblfihgt.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app\\static\\uploads')
+    # UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app\\static\\uploads')
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app', 'static', 'uploads')
